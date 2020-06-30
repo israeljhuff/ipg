974820565c5923a888f3b83dec373fd15bc2c85b
israeljhuff <israeljhuff@gmail.com>
2020-06-30 18:39:47 -0400

    - hiscript.grammar  . fixed bug: needed to move op_logical_binary check before op_arithmetic_binary to find || before | and && before &  . loop update now supports multiple updates  . removed expr_assign since expr_assign_multiple now replaces it everywhere  . added vector/map indexing to expressions  . changed map indexing syntax from {} to []
    - sample.hs
     . updates to sample code to reflect above changes
     . ported one of my FFT functions from C++ to flush out more bugs with real-world code

--- examples/sample.hs
+++ examples/sample.hs
@@ -14,12 +14,16 @@ bool what1 = true;
 bool what2 = false;
 
 {
-  if (foo > 10)
+  if (foo > 10 || bar < 5)
   {
     a += 5;
   }
   b += 5;
 }
+
+real[idx] = real[i];
+real[(a + 5) & 3] = real[(i + 7) ^ 5] + 1;
+real[a][3][!1] = real[b][~3][2] / foo[2];
 
 {
   uint64 a = 1234, b = 5;
@@ -70,6 +74,7 @@ loop (;;) {}
 loop (a = b - 7 * 5, c = d + 1, x = 5;;) {}
 loop (int32 a = b, c = d + 1, x = 5;;) {}
 loop (SomeClass a = 1, b = 2, c = 3;;) {}
+loop (int32 a = 1, b = 2, c = 3; a < 10; a += 1, b += 1, c += 1) {}
 
 vector<sint32> foo1 = [];
 vector<sint32> foo2 = [ 5 ];
@@ -80,10 +85,10 @@ vector<sint32> foo4 = [5,6];
 vector<sint32> foo2 = [2+7, asdf ];
 
 map<uint32, customType > bar = 1;  # parser doesn't do type checking so this is ok during parsing step of compilation
-map< uint32, vector < customType>> bar = {};
-map< uint32, vector < customType>> bar = {};
-map< uint32, vector < customType>> bar = {1:1};
-map< uint32, vector < customType>> bar = { 1 : 1, asdf : 5 + 7, };
+map< uint32, vector < customType>> bar = [];
+map< uint32, vector < customType>> bar = [];
+map< uint32, vector < customType>> bar = [1:1];
+map< uint32, vector < customType>> bar = [ 1 : 1, asdf : 5 + 7, ];
 
 void aVoidFunc()
 {
@@ -137,5 +142,57 @@ class MyClass
   private uint32 myFunc3(uint32 foo, sint8 bar)
   {
     return 3;
+  }
+}
+
+#------------------------------------------------------------------------------
+# NOTE: cos_tbl and sin_tbl LUTs assumed to be 1024 entries
+void fft_1d_no_recurse(vector<fp32> real, vector<fp32> imag, sint32 log2N)
+{
+  if (log2N < 0 || log2N > 31) { return; }
+
+  sint32 N = 1 << log2N;
+
+  # bit-reversal permutation
+  # rearrange date into order expected by FFT
+  loop (sint32 i = 0; i < N; i += 1)
+  {
+    sint32 idx = 0;
+    loop (sint32 j = 0; j < log2N; j += 1)
+    {
+      sint32 shift = (log2N - 1 - 2 * j);
+      if (shift >= 0) { idx += ((i & (1 << j)) << shift); }
+      else { idx += ((i & (1 << j)) >> -shift); }
+    }
+    if (idx <= i) { continue; }
+    fp32 swapr = real[idx];
+    fp32 swapi = imag[idx];
+    real[idx] = real[i];
+    imag[idx] = imag[i];
+    real[i] = swapr;
+    imag[i] = swapi;
+  }
+
+  # FFT
+  loop (sint32 Ni = 2, step = N / 2; Ni <= N; Ni *= 2, step /= 2)
+  {
+    fp32 scale_idx = 1024 / Ni;
+    loop (sint32 off = 0; off < N; off += Ni)
+    {
+      loop (sint32 k = 0; k < Ni / 2; k += 1)
+      {
+        sint32 idx1 = k + off;
+        sint32 idx2 = idx1 + Ni / 2;
+        sint32 index = k * scale_idx;
+        fp32 tmpr1 = real[idx1];
+        fp32 tmpi1 = imag[idx1];
+        fp32 tmpr2 = real[idx2];
+        fp32 tmpi2 = imag[idx2];
+        real[idx1] = tmpr1 + ( tmpr2 * cos_tbl[index] + tmpi2 * sin_tbl[index]);
+        imag[idx1] = tmpi1 + (-tmpr2 * sin_tbl[index] + tmpi2 * cos_tbl[index]);
+        real[idx2] = tmpr1 - ( tmpr2 * cos_tbl[index] + tmpi2 * sin_tbl[index]);
+        imag[idx2] = tmpi1 - (-tmpr2 * sin_tbl[index] + tmpi2 * cos_tbl[index]);
+      }
+    }
   }
 }
